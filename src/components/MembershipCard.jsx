1070452e595e72e6f47dfc73f688562a0fdeda23
adbertram <adbertram@gmail.com>
2025-05-02 08:44:11 -0500

    Fix linting issues in MembershipCard.jsx

--- src/components/MembershipCard.jsx
+++ src/components/MembershipCard.jsx
@@ -1,3 +1,4 @@
+// Fixed linting issues - 2025-05-02
 // Fixed linting issues - 2025-04-30
 // This component has quote style and indentation issues
 import React from "react";  // Double quotes instead of single
